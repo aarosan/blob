970c2cdcb75a14a598d2cb52c0a10e77828f0756
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-04-25 22:27:19 -0500

    React Signup Page
     - Styled the Signup Page with style.css
     - Signup Page is finished for MVP

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+import "./style.css";
+
+import RouterButton from "../components/routerButton";
+
 const Signup = () => {
+
+    const test = () => {
+        console.log('Enter Button Pressed');
+    }
     return (
         <React.Fragment>
             <Helmet>
@@ -9,8 +17,34 @@ const Signup = () => {
                 <meta />
             </Helmet>
 
-            <div className="page-content">
-                Signup!
+            <div className="signup-page-content">
+                <div className="title-container">
+                    sign up
+                </div>
+                <div className="input-container">
+                    <div className="first-name-container">
+                        <input type="text" className="first-name-input" placeholder="first name"/>
+                    </div>
+                    <div className="last-name-container">
+                        <input type="text" className="last-name-input"
+                        placeholder="last name" />
+                    </div>
+                    <div className="email-container">
+                        <input type="text" className="email-input" placeholder="email"/>
+                    </div>
+                    <div className="password-container">
+                        <input type="text" className="password-input"
+                        placeholder="password" />
+                    </div>
+                </div>
+                <div className="link-btn-container">
+                    <div className="btn-container">
+                        <RouterButton
+                        api="/"
+                        text="sign up" 
+                        onClick={test}/>
+                    </div>    
+                </div>
             </div>
         </React.Fragment>
     )
