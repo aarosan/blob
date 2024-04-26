00018241bdb6a2af7c179c3aec08f18c3d5b4328
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-04-26 18:27:10 -0500

    Connected Signup React Page to Server and DB
     - Added functionality to create new users in Signup.jsx
     - Successfully adds a user to the DB

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
+import Auth from '../utlis/auth';
 
 import "./style/style.css";
 
 import RouterButton from "../components/routerButton";
 
 const Signup = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const test = () => {
-        console.log('Enter Button Pressed');
-    }
+    const handleSignup = async () => {
+        console.log('Sign Up Button Pressed');
+
+        try {
+            console.log(firstName, lastName, email, password);
+            const response = await fetch("http://localhost:4000/users/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ firstName, lastName, email, password })
+            });
+
+            console.log('Response Status:', response.status);
+
+            console.log("Signup successful");
+
+            if (response.ok) {
+                const responseData = await response.json();
+                const token = responseData.token;
+    
+                Auth.login(token); 
+    
+            } else {
+                const errorData = await response.json();
+                throw new Error(errorData.message);
+            }
+    
+        } catch (error) {
+            console.error("Error signing up:", error.message);
+        }
+    };
+
     return (
         <React.Fragment>
             <Helmet>
@@ -23,18 +58,18 @@ const Signup = () => {
                 </div>
                 <div className="input-container">
                     <div className="first-name-container">
-                        <input type="text" className="first-name-input" placeholder="first name"/>
+                        <input type="text" className="first-name-input" placeholder="first name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
                     </div>
                     <div className="last-name-container">
                         <input type="text" className="last-name-input"
-                        placeholder="last name" />
+                        placeholder="last name" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                     </div>
                     <div className="email-container">
-                        <input type="text" className="email-input" placeholder="email"/>
+                        <input type="text" className="email-input" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                     <div className="password-container">
                         <input type="text" className="password-input"
-                        placeholder="password" />
+                        placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                 </div>
                 <div className="link-btn-container">
@@ -42,7 +77,7 @@ const Signup = () => {
                         <RouterButton
                         api="/"
                         text="sign up" 
-                        onClick={test}/>
+                        onClick={handleSignup}/>
                     </div>    
                 </div>
             </div>
--- client/src/utlis/auth.js
+++ client/src/utlis/auth.js
@@ -36,7 +36,7 @@ class AuthService {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
     window.location.assign('/myBlobs');
-  }
+  }  
 
   logout() {
     // Clear user token and profile data from localStorage
